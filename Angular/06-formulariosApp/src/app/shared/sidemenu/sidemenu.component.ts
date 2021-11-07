31b3c8a46ea95b12f3df635ba943346b1cc339f0
Sergio <sdamian.navarro@gmail.com>
2021-11-07 15:56:14 -0600

    FEAT: MODULO 16

---
+++ Angular/06-formulariosApp/src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,44 @@
+import { Component} from '@angular/core';
+
+interface MenuItem{
+  text: string
+  ruta: string
+}
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html'
+})
+export class SidemenuComponent{
+
+  templateMenu: MenuItem[] =[
+    {
+      text: "Basicos",
+      ruta: "./template/basicos"
+    },
+    {
+      text: "Dinámicos",
+      ruta: "./template/dinamicos"
+    },
+    {
+      text: "Switches",
+      ruta: "./template/switches"
+    }
+  ];
+
+  reactiveMenu: MenuItem[] =[
+    {
+      text: "Basicos",
+      ruta: "./reactive/basicos"
+    },
+    {
+      text: "Dinámicos",
+      ruta: "./reactive/dinamicos"
+    },
+    {
+      text: "Switches",
+      ruta: "./reactive/switches"
+    }
+  ];
+
+}
